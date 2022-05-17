4cb72d530137bc961fcf6134cf3e3c0a4de7bbea
binarypsilocybin <rogeriosvaldo@gmail.com>
2022-05-17 10:27:17 +0100

    1 file modified

--- index.js
+++ index.js
@@ -3,14 +3,7 @@ import './style.css';
 
 //https://medium.com/@andrey.igorevich.borisov/10-javascript-exercises-with-arrays-c44eea129fba
 
-/**
- * Task description: Write a method that creates a new array with given values
- * Expected Result: (3, 'a') => ['a', 'a', 'a']
- * Task Complexity: 1 of 5
- * @param {number} arraySize - size of array
- * @param {?} value - value to fill
- * @returns {Array}
- */
+//CREATE A NEW ARRAY WITH GIVEN VALUE
 const fill = (arraySize, value) => {
   return Array(arraySize).fill(value);
 };
@@ -19,15 +12,13 @@ const data = 3;
 const valueToFill = 'a';
 console.log(fill(data, valueToFill)); // ['a', 'a', 'a']
 
-/**
- * Task description: Write a method that reverts input array
- * Expected Result: [1, 2, 3] => [3, 2, 1]
- * Task Complexity: 1 of 5
- * @param {Array} array - Array of any elements
- * @returns {Array}
- */
+//REVERSE ARRAY
 const reverse = (array) => {
-  throw new Error('Put your solution here');
+  const reversed = [];
+  for (let i = array.length - 1; i >= 0; i -= 1) {
+    reversed.push(array[i]);
+  }
+  return reversed;
 };
-const data = [1, 2, 3];
-console.log(reverse(data)); // [3, 2, 1]
+const datas = [1, 2, 3];
+console.log(reverse(datas)); // [3, 2, 1]
